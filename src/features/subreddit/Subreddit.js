ccbf83daef7ece827314b6dcdb4a80f488997a44
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-11-06 17:53:57 +0100

    Fix React attribute naming

--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Subreddit({ subreddit }) {
   return (
-    <div class="flex items-center py-4">
+    <div className="flex items-center py-4">
       <img
         src={subreddit.icon}
         alt=""
-        class="w-6 h-6 mr-2 rounded-full"
+        className="w-6 h-6 mr-2 rounded-full"
       />
-      <p class="font-semibold">{subreddit.title}</p>
+      <p className="font-semibold">{subreddit.title}</p>
     </div>
   );
 }
