fb2c4cb850fcc153ed6af9f0aa2d6858ea310fce
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-11-06 17:47:50 +0100

    Fix React attribute naming

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -2,23 +2,26 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header class="fixed min-w-full shadow-lg bg-white">
-      <div class="container mx-auto px-4 h-20 flex justify-between items-center">
-        <div id="logo" class="flex items-center">
+    <header className="fixed min-w-full shadow-lg bg-white">
+      <div className="container mx-auto px-4 h-20 flex justify-between items-center">
+        <div id="logo" className="flex items-center">
           <img
-            class="w-8 fill-current text-green-600"
+            className="w-8 fill-current text-green-600"
             src="reddit-logo.svg"
             alt="reddit logo"
           />
-          <h1 class="pl-2 text-xl font-semibold">
-            <span class="font-bold">Reddit</span> Client
+          <h1 className="pl-2 text-xl font-semibold">
+            <span className="font-bold">Reddit</span> Client
           </h1>
         </div>
-        <div id="search" class="flex flex-1 mx-4 max-w-sm relative">
+        <div
+          id="search"
+          className="flex flex-1 mx-4 max-w-sm relative"
+        >
           <input
             type="search"
             placeholder="Search Reddit"
-            class="flex-1 border-solid border-2 border-gray-300 rounded-md pl-10 pr-4 py-2"
+            className="flex-1 border-solid border-2 border-gray-300 rounded-md pl-10 pr-4 py-2"
           ></input>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -38,25 +41,25 @@ export default function Header() {
 
         <div
           id="dark-mode-toggle"
-          class="hidden sm:flex border-solid border-2 border-gray-300 rounded-md divide-x-2 divide-gray-300 divide-solid"
+          className="hidden sm:flex border-solid border-2 border-gray-300 rounded-md divide-x-2 divide-gray-300 divide-solid"
         >
-          <div id="light-mode" class="px-4 py-2">
+          <div id="light-mode" className="px-4 py-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
               />
             </svg>
           </div>
-          <div id="dark-mode" class="px-4 py-2">
+          <div id="dark-mode" className="px-4 py-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
