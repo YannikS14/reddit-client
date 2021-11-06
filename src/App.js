9b7b62ba5fd78b8369f0f3c069bb8c79cc027b31
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-11-06 17:48:52 +0100

    Add redux logic Move initial state from props to redux

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './features/header/Header';
-import PostList from './features/postList/PostList';
-import SubredditList from './features/subredditList/SubredditList';
+import Posts from './features/posts/Posts';
+import Subreddits from './features/subreddits/Subreddits';
 
 function App() {
   return (
     <div className="App bg-gray-100 min-h-screen">
       <Header />
-      <main class="container mx-auto pt-24 flex flex-col sm:flex-row">
-        <PostList
-          posts={[
-            {
-              title: 'postTitle',
-              votes: '35.8k',
-              content: 'the content',
-              image: '',
-              postedBy: 'author',
-              publishDate: '8 hours ago',
-              commentsAmount: 345,
-            },
-            {
-              title: 'postTitle',
-              votes: '35.8k',
-              content: '',
-              image: 'https://i.redd.it/swuqoroqfox71.jpg',
-              postedBy: 'author',
-              publishDate: '8 hours ago',
-              commentsAmount: 345,
-            },
-            {
-              title: 'postTitle',
-              votes: '35.8k',
-              content: 'the content',
-              image: '',
-              postedBy: 'author',
-              publishDate: '8 hours ago',
-              commentsAmount: 345,
-            },
-          ]}
-        />
-        <SubredditList
-          subreddits={[
-            {
-              icon: 'https://b.thumbs.redditmedia.com/EndDxMGB-FTZ2MGtjepQ06cQEkZw_YQAsOUudpb9nSQ.png',
-              title: 'AskReddit',
-            },
-            {
-              icon: 'https://b.thumbs.redditmedia.com/0PgZl68jAxA6T1BH6uvUQ5Bz1F1GrrJLCL8oi2Gz0Ak.png',
-              title: 'gaming',
-            },
-          ]}
-        />
+      <main className="container mx-auto pt-24 flex flex-col sm:flex-row">
+        <Posts />
+        <Subreddits />
       </main>
     </div>
   );
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import postsReducer from '../features/posts/postsSlice';
+import subredditReducer from '../features/subreddits/subredditsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    posts: postsReducer,
+    subreddits: subredditReducer,
   },
 });
