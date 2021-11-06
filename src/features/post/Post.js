1286cc3d42d3b1f986fb9c2ca88c58e3245b430c
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-11-06 17:50:46 +0100

    Change props to fit the reddit api response Add momentjs for date formatting Reformat numbers > 999 with k-suffix

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,9 +1,20 @@
+import moment from 'moment';
 import React from 'react';
 
 export default function Post({ post }) {
+  const kFormatter = (num) => {
+    return Math.abs(num) > 999
+      ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k'
+      : Math.sign(num) * Math.abs(num);
+  };
+
+  const postDate = moment(post.data.created * 1000).fromNow();
+  const upvotes = kFormatter(post.data.ups);
+  const commentsAmount = kFormatter(post.data.num_comments);
+
   return (
-    <article class="mb-8 p-6 bg-white rounded-md shadow-lg flex">
-      <div id="votes" class="pr-6 flex flex-col items-center">
+    <article className="mb-8 p-6 bg-white rounded-md shadow-lg flex">
+      <div id="votes" className="pr-6 flex flex-col items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -18,7 +29,7 @@ export default function Post({ post }) {
             d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
           />
         </svg>
-        <p>{post.votes}</p>
+        <p>{upvotes}</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -34,17 +45,22 @@ export default function Post({ post }) {
           />
         </svg>
       </div>
-      <div id="post-content" class="flex-1">
-        <h3 class="mb-4 text-xl font-semibold">{post.title}</h3>
-        {post.image && (
-          <img src={post.image} alt="" class="rounded-md" />
+      <div id="post-content" className="flex-1">
+        <h3 className="mb-4 text-xl font-semibold">
+          {post.data.title}
+        </h3>
+        {post.data.url && (
+          <img src={post.data.url} alt="" className="rounded-md" />
         )}
-        {post.content && <p class="mt-4">{post.content}</p>}
-        <hr class="mt-4" />
-        <div id="post-footer" class="flex justify-between px-8 py-2">
-          <div class="font-semibold">{post.postedBy}</div>
-          <div>{post.publishDate}</div>
-          <div class="flex">
+        {post.content && <p className="mt-4">{post.content}</p>}
+        <hr className="mt-4" />
+        <div
+          id="post-footer"
+          className="flex justify-between px-8 py-2"
+        >
+          <div className="font-semibold">{post.data.author}</div>
+          <div>{postDate}</div>
+          <div className="flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 mr-1"
@@ -59,7 +75,7 @@ export default function Post({ post }) {
                 d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
               />
             </svg>
-            {post.commentsAmount}
+            {commentsAmount}
           </div>
         </div>
       </div>
