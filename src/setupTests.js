9beec83a85b59c2228243240526c7a1dd0363611
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-11-25 14:41:42 +0100

    Setup tests for react testing library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,3 @@
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-configure({ adapter: new Adapter() });
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect';
